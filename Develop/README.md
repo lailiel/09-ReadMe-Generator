3fe42217e41f92da456ce7ede96c7805a8bed901
Abi L <124858748+lailiel@users.noreply.github.com>
2023-05-13 15:21:35 -0500

    testing appearence of markdown in github, license stuff still needs to be fixed

--- Develop/README.md
+++ Develop/README.md
@@ -1,12 +1,34 @@
-# d
-  This is a test,
-  d:
-  d:
-  d:
-  d:
-  d:
-  d:
-  MIT:
-  d:
-  d:
-  this is the end of the test.
+## b
+
+  b
+  
+## Table of Contents
+
+  1. [Installation](#install)
+  2. [Usage](#usage)
+  3. [License](#license)
+  4. [Contribution](#contribution)
+  5. [Testing](#test)
+
+## Installation
+
+  b
+
+## Usage
+
+  b
+
+## License
+
+  
+
+## Contribution
+
+  b
+
+## Testing
+
+  b
+
+## Contacts
+  Email me at [b](mailto:b) or explore more projects at[b](https://www.github.com/b)
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -2,8 +2,8 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (license == true) 
-  return `[!(https://img.shields.io/badge/license-${license}-blue)]` 
-  else return ''
+  return "[!(https://img.shields.io/badge/license-${license}-blue)]"
+  else return ""
 }
 
 
@@ -12,37 +12,39 @@ function renderLicenseBadge(license) {
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
 
-  if (license === 'MIT')
-   return `https://opensource.org/license/mit/`
-  else if (license === 'APACHE 2.0')
-   return `https://opensource.org/license/apache-2-0/`
-  else if (license === 'GPL 3.0')
-   return `https://opensource.org/license/gpl-3-0/`
-  else if (license === 'BSD 3')
-   return `https://opensource.org/license/bsd-3-clause/`
-  else return ''
+  if (license == 'MIT')
+   return "https://opensource.org/license/mit/"
+  else if (license == 'APACHE 2.0')
+   return "https://opensource.org/license/apache-2-0/"
+  else if (license == 'GPL 3.0')
+   return "https://opensource.org/license/gpl-3-0/"
+  else if (license == 'BSD 3')
+   return "https://opensource.org/license/bsd-3-clause/"
+  else return ""
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  renderLicenseBadge()
-  renderLicenseLink()
+ const badge = renderLicenseBadge()
+ const link = renderLicenseLink()
   if (license == true)
-   return `Project is licensed under [${badge}](${licenseLink})`
+   return "Project is licensed under ${badge}(${link})"
   else 
-   return ''
- 
-
+   return ""
 }
+
+
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  const licenseSection = renderLicenseSection()
-  const {github, email, project, description, install, usage, contribution, test} = data
+  const {github, email, project, description, install, usage, license, contribution, test} = data
+  const licenseSec = renderLicenseSection(license)
   return `## ${project}
+
   ${description}
-  ## Table of Contents
+  
+## Table of Contents
 
   1. [Installation](#install)
   2. [Usage](#usage)
@@ -50,27 +52,27 @@ function generateMarkdown(data) {
   4. [Contribution](#contribution)
   5. [Testing](#test)
 
-  ## Installation
+## Installation
 
   ${install}
 
-  ## Usage
+## Usage
 
   ${usage}
 
-  ## License
+## License
 
-  ${licenseSection}
+  ${licenseSec}
 
-  ## Contribution
+## Contribution
 
   ${contribution}
 
-  ## Testing
+## Testing
 
   ${test}
 
-  ## Contacts
+## Contacts
   Email me at [${email}](mailto:${email}) or explore more projects at[${github}](https://www.github.com/${github})
 `;
 }
